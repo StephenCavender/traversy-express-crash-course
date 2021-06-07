c9b68e9d2b7fc60ae6c0ff847af54b145129b2be
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-06-07 12:42:12 -0400

    working with static files / folders

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello!");
-});
+// app.get("/", (req, res) => {
+//   // res.send("hello!");
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
