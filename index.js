78f5d8b97a8f97b4c720ecd48492c35612a947f2
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-06-07 14:05:24 -0400

    pull out logic into routes folder

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 
-const members = require("./Members");
 const logger = require("./middleware/logger");
 
 const app = express();
@@ -14,22 +13,11 @@ const app = express();
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 
-// Gets all members
-app.get("/api/members", (req, res) => res.json(members));
-
-// Get single member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with id ${req.params.id}` });
-  }
-});
-
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
