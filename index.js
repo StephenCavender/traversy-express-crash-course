3d73dc6a3445fce14f7333783f736ec30365debb
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-06-07 14:40:43 -0400

    add main layout and view using handlebars

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-const logger = require("./middleware/logger");
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // init logger
 // app.use(logger);
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Home page route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members,
+  })
+);
 
 // app.get("/", (req, res) => {
 //   // res.send("hello!");
@@ -18,10 +31,10 @@ app.use(express.urlencoded({ extended: false }));
 // });
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Members API routes
-app.use("/api/members", require("./routes/api/members"));
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
